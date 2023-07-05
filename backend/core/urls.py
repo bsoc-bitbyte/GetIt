5203d5377d9f80ac6a28249bd12607513775a567
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-07-05 20:39:50 +0530

    updated url to incorporate routes

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework import routers
 
-urlpatterns = [
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('api/token/', include('auth.urls')),
 ]
