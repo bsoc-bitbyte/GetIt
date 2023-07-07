1118dec5b8a6e8143428ae57db66daf06a79020c
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-07-07 16:19:56 +0530

    Removed username field

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -4,7 +4,7 @@ from .models import Account
 # Register your models here.
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active', 'is_staff')
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_active', 'is_staff')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,16 +4,12 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self, first_name, last_name, email, password=None):
         if not email:
             raise ValueError("Users must have an email address")
         
-        if not username:
-            raise ValueError("Users must have a username")
-        
         user = self.model(
             email = self.normalize_email(email),
-            username = username,
             first_name = first_name,
             last_name = last_name
         )
@@ -23,10 +19,9 @@ class MyAccountManager(BaseUserManager):
 
         return user
     
-    def create_superuser(self, first_name, last_name, username, email, password):
+    def create_superuser(self, first_name, last_name, email, password):
         user = self.create_user(
             email = self.normalize_email(email),
-            username = username,
             first_name = first_name,
             last_name = last_name,
             password = password
@@ -46,7 +41,6 @@ class MyAccountManager(BaseUserManager):
 class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    username = models.CharField(unique=True, max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=50)
 
@@ -59,7 +53,7 @@ class Account(AbstractBaseUser):
     is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     objects = MyAccountManager()
 
