8c61b12c7c2d74b0cfed25244406ce44f854a2d3
Priyansh61 <21bec080@iiitdmj.ac.in>
2023-07-07 16:29:37 +0530

    removed routers

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from rest_framework import routers
 
-router = routers.DefaultRouter()
-
-urlpatterns = router.urls
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', include('auth.urls')),
 ]
