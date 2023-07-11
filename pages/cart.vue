949896c866b3ec199a0385a03280b4241b9d7d34
Kushagra <22bcs148@iiitdmj.ac.in>
2023-07-11 23:10:15 +0530

    Made the required changes

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="main h-full flex items-center justify-center bg-white-100 ">
+    <div class="main h-full flex items-center justify-center bg-white-100 m-16">
 
         <div class="cart h-full w-full bg-white-100 ">
 
             <div class="items-left" style=" width: 35rem; margin-left: auto; margin-right: auto;">
-                <h3 class=" flex font-Poppins text-xl font-bold py-8 px-12 " style="width: 19.417rem;">Shopping Cart</h3>
+                <h3 class=" flex font-Poppins text-xl font-bold py-8 px-12 " style="width: 19.417rem; top: 1rem;">Shopping Cart</h3>
             </div>    
             
-            <div class=" flex-col items-center justify-around h-full w-full  ">
-
+            <div class=" flex-col items-center justify-around h-full w-full " 
+                v-for=" (product, index ) in products" :key=" index " >
+                
                 <div id="selected_prod1" class=" h-32 flex justify-evenly border-2 item_1 rounded-lg mt-8" style=" width: 35rem; margin-left: auto; margin-right: auto;">
 
                     <div class="image_div flex items-center justify-center"> 
@@ -19,19 +20,19 @@
 
                     <div class="prod_Description w-40 h-28 mt-6 flex-col justify-center ">
 
-                        <h3 id="prod_Name" class="font-Poppins text-lg font-semibold leading-6" style="color: #271819;"> PRODUCT NAME </h3>
-                        <h4 id="prod_Price" class="font-Mulish text-lg font-bold leading-6" style="color: #C3C6C9;">69$</h4>
-                        <p id="prod_Size" class="font-Mulish text-lg font-semibold leading-12" style="color: #271819;">Size : M </p>
+                        <h3 id="prod_Name" class="font-Poppins text-lg font-semibold leading-6" style="color: #271819;">{{ product.ProductName }}</h3>
+                        <h4 id="prod_Price" class="font-Mulish text-lg font-bold leading-6" style="color: #C3C6C9;">{{ product.ProductPrice }}</h4>
+                        <p id="prod_Size" class="font-Mulish text-lg font-semibold leading-12" style="color: #271819;">{{ product.ProductSize }}</p>
 
                     </div>
 
                     <div class="Quantity flex justify-evenly w-28 h-28 mt-2 "> 
 
-                        <button onclick="" class="flex items-center justify-cente p-2 w-7 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">-</button>
+                        <button onclick="" class="flex items-center justify-cente p-2 w-7 h-8 mt-10 text-black rounded-lg border-2 " style="background-color: #EEF1F4;">-</button>
 
-                        <p id="count" class="flex items-center justify-cente p-2 h-8 mt-10 text-black "> 1 </p>
+                        <p id="count" class="flex items-center justify-cente p-2 h-8 mt-10 " style="color: #C3C6C9; "> 1 </p>
 
-                        <button onclick="" class="flex items-center justify-cente p-2 w-8 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">+</button>
+                        <button onclick="" class="flex items-center justify-cente p-2 w-8 h-8 mt-10 text-black rounded-lg border-2 "  style="background-color: #EEF1F4;">+</button>
 
                     </div>
 
@@ -40,103 +41,6 @@
                     </div>
 
                 </div>
-
-                <div id="selected_prod1" class=" h-32 flex justify-evenly border-2 item_1 rounded-lg mt-8" style=" width: 35rem; margin-left: auto; margin-right: auto;">
-
-                    <div class="image_div flex items-center justify-center"> 
-
-                        <img src="../assets/get_it.png" alt="Image description" class="w-24 h-24 object-cover">
-
-                    </div>
-
-                    <div class="prod_Description w-40 h-28 mt-6 flex-col justify-center ">
-
-                        <h3 id="prod_Name" class="font-Poppins text-lg font-semibold leading-6" style="color: #271819;"> PRODUCT NAME </h3>
-                        <h4 id="prod_Price" class="font-Mulish text-lg font-bold leading-6" style="color: #C3C6C9;">69$</h4>
-                        <p id="prod_Size" class="font-Mulish text-lg font-semibold leading-12" style="color: #271819;">Size : M </p>
-
-                    </div>
-
-                    <div class="Quantity flex justify-evenly w-28 h-28 mt-2 "> 
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-7 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">-</button>
-
-                        <p id="count" class="flex items-center justify-cente p-2 h-8 mt-10 text-black "> 1 </p>
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-8 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">+</button>
-
-                    </div>
-
-                    <div class="close_btn flex-row justify-end w-8 ">
-                        <button class="flex-row justify-end ml-8 mt-2">X</button>
-                    </div>
-
-                </div>
-
-                <div id="selected_prod1" class=" h-32 flex justify-evenly border-2 item_1 rounded-lg mt-8" style=" width: 35rem; margin-left: auto; margin-right: auto;">
-
-                    <div class="image_div flex items-center justify-center"> 
-
-                        <img src="../assets/get_it.png" alt="Image description" class="w-24 h-24 object-cover">
-
-                    </div>
-
-                    <div class="prod_Description w-40 h-28 mt-6 flex-col justify-center ">
-
-                        <h3 id="prod_Name" class="font-Poppins text-lg font-semibold leading-6" style="color: #271819;"> PRODUCT NAME </h3>
-                        <h4 id="prod_Price" class="font-Mulish text-lg font-bold leading-6" style="color: #C3C6C9;">69$</h4>
-                        <p id="prod_Size" class="font-Mulish text-lg font-semibold leading-12" style="color: #271819;">Size : M </p>
-
-                    </div>
-
-                    <div class="Quantity flex justify-evenly w-28 h-28 mt-2 "> 
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-7 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">-</button>
-
-                        <p id="count" class="flex items-center justify-cente p-2 h-8 mt-10 text-black "> 1 </p>
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-8 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">+</button>
-
-                    </div>
-
-                    <div class="close_btn flex-row justify-end w-8 ">
-                        <button class="flex-row justify-end ml-8 mt-2">X</button>
-                    </div>
-
-                </div>
-
-                <div id="selected_prod1" class=" h-32 flex justify-evenly border-2 item_1 rounded-lg mt-8" style=" width: 35rem; margin-left: auto; margin-right: auto;">
-
-                    <div class="image_div flex items-center justify-center"> 
-
-                        <img src="../assets/get_it.png" alt="Image description" class="w-24 h-24 object-cover">
-
-                    </div>
-
-                    <div class="prod_Description w-40 h-28 mt-6 flex-col justify-center ">
-
-                        <h3 id="prod_Name" class="font-Poppins text-lg font-semibold leading-6" style="color: #271819;"> PRODUCT NAME </h3>
-                        <h4 id="prod_Price" class="font-Mulish text-lg font-bold leading-6" style="color: #C3C6C9;">69$</h4>
-                        <p id="prod_Size" class="font-Mulish text-lg font-semibold leading-12" style="color: #271819;">Size : M </p>
-
-                    </div>
-
-                    <div class="Quantity flex justify-evenly w-28 h-28 mt-2 "> 
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-7 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">-</button>
-
-                        <p id="count" class="flex items-center justify-cente p-2 h-8 mt-10 text-black "> 1 </p>
-
-                        <button onclick="" class="flex items-center justify-cente p-2 w-8 h-8 mt-10 text-black rounded-lg border-2  bg-slate-300">+</button>
-
-                    </div>
-
-                    <div class="close_btn flex-row justify-end w-8 ">
-                        <button class="flex-row justify-end ml-8 mt-2">X</button>
-                    </div>
-
-                </div>
-
             </div>
             
 
@@ -144,24 +48,35 @@
 
         <div class="billing h-full w-full flex-col items-center justify-center ">
 
-            <div class="girl_img w-full flex items-center justify-center mt-32 " style="height: 16rem;">
+            <div class="girl_img w-full h-1/2 flex items-center justify-center mt-32 ">
 
                 <img class="h-full w-3/5 block" src="../assets/cart_girl.png" style=" margin-left: auto; margin-right: auto;">
 
             </div>
 
-            <div class="Bill flex items-center justify-center w-full bg-white-100 mt-4" style="height: 21rem;"> 
+            <div class="Bill flex items-center justify-center w-full h-70 bg-white-100 mt-4"> 
 
                 <div class=" w-3/5 h-full border-2 bg-white-100" style="width: 26.75rem"> 
-
-                    <h3 class="font-medium text-2xl h-1/6 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Subtotal :</h3>
+                    <div class="flex">
+                    <h3 class="font-medium text-2xl h-1/6 w-1/2 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Subtotal :</h3>
+                    <div class="font-Mulish text-lg w-1/2 font-bold leading-6 ml-40 mt-6" style="color: #C3C6C9;"> 900$ </div>
+                    </div>
                     <hr class="w-11/12 ml-2">
-                    <h3 class="font-medium text-2xl h-1/6 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Discount :</h3>
+                    <div class="flex"> 
+                    <h3 class="font-medium text-2xl h-1/6 w-1/2 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: 
+                    normal; line-height: normal; letter-spacing: 0.04125rem;">Discount :</h3>
+                    <div class="font-Mulish text-lg w-1/2 font-bold leading-6 ml-40 mt-6" style="color: #C3C6C9;"> 0$ </div>
+                    </div>
                     <hr class="w-11/12 ml-2">
-                    <h3 class="font-medium text-2xl h-1/6 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Tax :</h3>
+                    <div class="flex"> 
+                    <h3 class="font-medium text-2xl h-1/6 my-2 w-1/2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Tax :</h3>
+                    <div class="font-Mulish text-lg w-1/2 font-bold leading-6 ml-40 mt-6" style="color: #C3C6C9;"> 5$ </div>
+                    </div>
                     <hr class="w-11/12 ml-2">
-                    <h3 class="font-medium text-2xl h-1/6 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Total :</h3>
-
+                    <div class="flex"> 
+                    <h3 class="font-medium text-2xl h-1/6 w-1/2 my-2 mt-6 ml-8 text-black font-Poppins " style="font-family: Poppins; font-weight: 800; font-style: normal; line-height: normal; letter-spacing: 0.04125rem;">Total :</h3>
+                    <div class="font-Mulish text-lg w-1/2 font-bold leading-6 ml-40 mt-6" style="color: #C3C6C9;"> 905$ </div>
+                    </div>    
                 </div>
 
             </div>
@@ -178,7 +93,18 @@
 </template>
 
 <script>
-  export default {
+    export default {
+        data: () => {
+            return {
+                products: [
+                    {ProductName: 'Product1', ProductPrice: '69$', ProductSize: 'M'},
+                    {ProductName: 'Product2', ProductPrice: '69$', ProductSize: 'M'},
+                    {ProductName: 'Product3', ProductPrice: '69$', ProductSize: 'M'},
+                    {ProductName: 'Product4', ProductPrice: '69$', ProductSize: 'M'}
+                ]
+            }
+        },
     name:'cart'
   }
 </script>
+
