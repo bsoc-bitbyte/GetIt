66aafbc6011f19a93ac614a7e5a28c2aabfc0cc0
Gaurangi Bansal <bansal0410.gaurangi@gmail.com>
2023-07-16 00:21:11 +0530

    Fixed the image dimensions

--- pages/Signin.vue
+++ pages/Signin.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main min-[1120px]:flex min-[1120px]:justify-evenly py-[2rem] px-[0.1rem] mt-[3rem]">
-    <div class="left-panel mt-[2.69rem] max-[1119px]:hidden min-[1120px]:block">
-      <img src="../assets/37.order-delivered-3.png" class = "w-[32rem] h-[28rem] border-1 border-gray-400 min-[1120px]:order-1" alt="delivery-img">
+    <div class="left-panel mt-[5.5rem] max-[1119px]:hidden min-[1120px]:block">
+      <img src="../assets/37.order-delivered-3.png" class = "w-[34rem] h-[32rem] border-1 border-gray-400 min-[1120px]:order-1" alt="delivery-img">
     </div>
     <div class="right-panel max-[1119px]:px-[2rem] min-[1120px]:px-[0rem]">
       <div class="form-container">
@@ -9,20 +9,20 @@
           <div class="credentials - container min-[1120px]:order-2 min-[1120px]:mt-[3rem]">
         <div class="catch-line-outer">
             <div class="catch-line-inner">
-                <p class = "text-slate-500 font-semibold">"Welcome back! Please enter your details"</p>
+                <p class = "text-slate-500 font-semibold py-[2rem]">"Welcome back! Please enter your details"</p>
             </div>
       </div>
      <div class="credentials">
-      <div class="email py-[0.5rem]">
+      <div class="email py-[1.0rem]">
           <p class = "font-bold subpixel-antialiased text-slate-500/[.98] py-[0.1rem] tracking-wide text-slate-800 ">Email</p>
           <input type="email" class = "px-[1rem] border-2 h-[2.69rem] border-black-300 max-[1119px]:w-full min-[1120px]:w-[35rem] rounded-lg" v-model.trim.lazy = "credentials.email" name="" placeholder="Enter your email"/>
         </div>
-        <div class="password  py-[0.5rem]">
+        <div class="password  py-[1.0rem]">
           <p class = "font-bold subpixel-antialiased text-slate-500/[.98] tracking-wide py-[0.1rem] text-slate-800">Password</p>
           <input type="password"  class = "px-[1rem] border-2 h-[2.69rem] border-black-300 max-[1119px]:w-full min-[1120px]:w-[35rem] rounded-lg" v-model.trim.lazy = "credentials.password" name="" placeholder="***********"/>
         </div>
      </div>
-        <div class="remember-forgot py-[0.5rem] flex justify-between">
+        <div class="remember-forgot py-[1.0rem] flex justify-between">
           <div class="remember-me">
             <input type="checkbox" name="check" id="checkbox" true-value = "yes" false-value = "no" v-model.trim.lazy="credentials.rememberMe">
             <label for="checkbox" class = " subpixel-antialiased font-bold text-slate-800 cursor-pointer">Remember me</label>
