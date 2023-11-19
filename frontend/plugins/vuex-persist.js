887620d32866c1cbe333aef149d9ef3f935d3ce3
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-11-19 20:03:07 +0530

    minor file movements

---
+++ frontend/plugins/vuex-persist.js
@@ -0,0 +1,18 @@
+export default ({ store }) => {
+  if (process.client) {
+    window.onNuxtReady(() => {
+      // Load cart data from localStorage, if available
+      const cartData = localStorage.getItem('cart');
+      if (cartData) {
+        store.commit('setCart', JSON.parse(cartData));
+      }
+
+      // Save the cart data to localStorage whenever the cart state changes
+      store.subscribe((mutation, state) => {
+        if (mutation.type === 'setCart') {
+          localStorage.setItem('cart', JSON.stringify(state.cart));
+        }
+      });
+    });
+  }
+};
