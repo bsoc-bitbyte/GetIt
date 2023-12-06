514348f054612bb10451d7e7b04eb64f168105a5
ChaudharyRaman <ramanchaudhary2704@gmail.com>
2023-12-06 19:52:17 +0530

    deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +15,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('DJANGO_SECRET', 'default')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', "False").lower == 'true'
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
@@ -95,10 +96,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        # dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 }
 
-
+DATABASES['default'] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
