90efb6f149372b0aa5e8ce7cc5ede7f90a5b4092
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2023-12-29 15:24:56 +0530

    remove depcrecated plugin vuex-persistedstate

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false }
+    { src: '~/plugins/vuex-persist', ssr: false },
+    { src: '~/plugins/local-storage', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -45,10 +46,8 @@ export default {
     'nuxt-material-design-icons-iconfont',
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'https://backend-getit.onrender.com/api/',
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
