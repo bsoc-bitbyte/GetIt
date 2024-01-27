ac84941e9ca27c7ae65ecb6bfa70ae458297e823
Armin Patel <72291325+arminpatel@users.noreply.github.com>
2024-01-28 00:15:23 +0530

    fix : Update build.py (#122)
    * add login signup and logout functionality
    
    * remove unwanted files
    
    * update build file and fix key error

--- backend/core/views.py
+++ backend/core/views.py
@@ -9,7 +9,9 @@ from tickets.models import Ticket
 @csrf_exempt
 def stripe_webhook(request) :
     payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    sig_header = request.META.get('HTTP_STRIPE_SIGNATURE', None)
+    if not sig_header:
+        return HttpResponse(status=400)
     event = None
 
     try :
