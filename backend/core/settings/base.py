2e29c1b038e20211a0348731d82039f8fa4601f7
Bhavik Agarwal <73033511+Bhavik-ag@users.noreply.github.com>
2024-03-07 10:19:55 +0530

    feat: added swagger api docs (#132)
    * added swagger api docs
    
    * Update base.py

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
     'core',
     'accounts',
     'products',
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,8 +3,21 @@ from django.urls import path
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .views import stripe_webhook
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="GetIt API",
+      default_version='v1',
+      description="API Documentation"
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +26,5 @@ urlpatterns = [
     path('api/events/', include('events.urls')),
     path('api/tickets/', include('tickets.urls')),
     path('api/stripe/webhook/', stripe_webhook, name='stripe-webhook'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
