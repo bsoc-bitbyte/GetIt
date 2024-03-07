ce57a3c1b8fb5e4708514b8393a8883d1d523f03
tushit03 <tushit.shukla03@gmail.com>
2024-03-07 09:41:11 +0530

    Made cart and migrated eventList from home page

--- frontend/pages/cart.vue
+++ frontend/pages/cart.vue
@@ -1,5 +1,5 @@
-<template>
-    <div class="main h-full flex flex-col md:flex-row items-start justify-center bg-white-100 mt-3 lg:mt-0 xl:mt-8 m-4 lg:m-10 xl:m-16">
+<template >
+    <!-- <div class="main h-full flex flex-col md:flex-row items-start justify-center bg-white-100 mt-3 lg:mt-0 xl:mt-8 m-4 lg:m-10 xl:m-16">
 
         <div class="cart flex flex-col h-auto w-auto lg:w-[35rem] xl:w-[35rem] bg-white-100 items-center justify-center md:items-start">
             
@@ -96,8 +96,86 @@
             </div>
         </div>
 
-    </div>
+    </div> -->
+<!-- <div class="flex flex-row justify-between"> -->
+<section class="h-screen  py-12 sm:py-16 lg:py-20 scale-110">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8 flex flex-row">
+    <div class="mx-auto mt-8 max-w-2xl md:mt-12">
+      <div class="bg-white shadow">
+        <div class="px-4 py-6 sm:px-8 sm:py-10">
+          <div class="flow-root">
+            <ul class="-my-8" v-for="item in $store.state.cart" :key="item.pid">
+              <li class="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+                <div class="shrink-0">
+                  <img class="h-24 w-24 max-w-full rounded-lg object-cover" :src="item.cover_image" alt="" />
+                </div>
 
+                <div class="relative flex flex-1 flex-col justify-between">
+                  <div class="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                    <div class="pr-8 sm:pr-5">
+                      <p class="text-base font-semibold text-gray-700">{{ item.title }}</p>
+                      <p class="mx-0 mt-1 mb-0 text-sm text-gray-400">{{ item.location }}</p>
+                    </div>
+
+                    <div class="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                      <p class="shrink-0 w-20 text-base font-semibold text-gray-700 sm:order-2 sm:ml-8 sm:text-right">${{ item.totalPrice }}</p>
+
+                      <div class="sm:order-1">
+                        <div class="mx-auto flex h-8 items-stretch text-gray-600">
+                          <button @click="decreaseQ(item)" class="flex items-center justify-center rounded-l-md bg-gray-200 px-4 transition hover:bg-black hover:text-white">-</button>
+                          <div class="flex w-full items-center justify-center bg-gray-100 px-4 text-xs uppercase transition">{{ item.quantity }} </div>
+                          <button @click="increaseQ(item)" class="flex items-center justify-center rounded-r-md bg-gray-200 px-4 transition hover:bg-black hover:text-white">+</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+                    <button @click.prevent="removeFromCart(item)" type="button" class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-700">
+                      <svg  class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" class=""></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </li> 
+            </ul>
+          </div>
+
+          <div class="mt-6 border-t border-b py-2">
+            <div class="flex items-center justify-between">
+              <p class="text-sm text-gray-400">Subtotal</p>
+              <p class="text-lg font-semibold text-gray-700">₹{{ $store.getters['getPrice'] }}</p>
+            </div>
+            <div class="flex items-center justify-between">
+              <p class="text-sm text-gray-400">Shipping</p>
+              <p class="text-lg font-semibold text-gray-700">₹ 0 </p>
+            </div>
+          </div>
+          <div class="mt-6 flex items-center justify-between">
+            <p class="text-sm font-medium text-gray-700">Total</p>
+            <p class="text-2xl font-semibold text-gray-700"><span class="text-xs font-normal text-gray-400">RS</span>{{ $store.getters['getPrice'] }}</p>
+          </div>
+
+          <div class="mt-6 text-center">
+            <button type="button" class="group inline-flex w-full items-center justify-center rounded-md bg-[#EA454C] px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow">
+              Checkout
+              <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-6 w-6 transition-all" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  
+</section>
+<!-- <div class="h-screen  py-12 mt-5 sm:py-16 lg:py-20 ">
+    <img class="h-[50vh] px-32 hidden " src="../assets/cart_girl.png">
+</div>
+</div> -->
 </template>
 
 <script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,54 +1,18 @@
 <template>
-  <div class="m-20">
-    <h1 class="text-4xl font-bold text-center">Event List</h1>
-    <div class="flex justify-center items-center flex-col gap-8 mt-20 lg:flex-row">
-      <nuxt-link v-for="(data, index) in eventLists" :to="'events/'+ (index + 1)" key="index"><eventList
-        :key="index"
-        :title="data.title"
-        :description="data.description"
-        :email="data.email"
-        :date="data.date"
-        :event_type="data.event_type"
-        :location="data.location"
-        :ticket_price="data.ticket_price"
-        :img_url="data.cover_image"
-      /></nuxt-link>
-      
-    </div>
-  </div>
+  
 </template>
 
 <script>
-import eventList from '@/components/eventList.vue';
+
 
 
 export default {
   name: 'IndexPage',
-  data() {
-    return {
-      eventLists: [],
-    };
-  },
-  components: {
-    eventList,
-  },
-
-  
-  
   mounted() {
-    this.fetchEventData();
+   
   },
   methods: {
-    async fetchEventData() {
-      try {
-        const response = await this.$axios.get('events/');
-        this.eventLists = response.data;
-     
-      } catch (error) {
-        console.error('Error fetching event data', error);
-      }
-    },
-  },
-};
+    
+}};
 </script>
 
