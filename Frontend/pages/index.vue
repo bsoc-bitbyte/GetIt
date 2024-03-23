38beebf98cd492ac3cdc01ab2e91f393640b41c2
tushit03 <tushit.shukla03@gmail.com>
2024-03-24 01:34:36 +0530

    landing page

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="flex justify-center items-center h-[80vh] ml-48 overflow-y-hidden">
+  <div class="justify-center items-center h-[80vh] ml-48 overflow-y-hidden hidden lg:flex">
     <div class="">
       <p class="text-xl text-gray-600 mb-2">Don't miss out on the Events! Get your tickets now.</p>
       <router-link to="/eventList">
-        <button class="btn bg-[#ea454c] text-white w-[10vw] h-[5vh] rounded-3xl"
-                    >Explore events
+        <button class="btn bg-[#ea454c] text-white w-48 h-12 rounded-3xl">Explore events
                   </button>    
       </router-link>
     </div>
@@ -13,12 +12,29 @@
         <Vue3Lottie 
           animationLink="https://lottie.host/d825e1b5-1261-4260-af09-953428d23a6a/eKnQiaIKbM.json"
           :height="1000"
-
         />
       </client-only>
     </div>
-    
   </div>
+
+
+    <div class="flex flex-col justify-center items-center h-[50vh] lg:hidden">
+      <div class="animation-container ">
+      <client-only>
+        <Vue3Lottie 
+          animationLink="https://lottie.host/d825e1b5-1261-4260-af09-953428d23a6a/eKnQiaIKbM.json"
+          :height="400"
+        />
+      </client-only>
+    </div>
+      <div class="text-center">
+        <p class="text-xl text-gray-600 mb-2">Don't miss out on the Events! Get your tickets now.</p>
+        <router-link to="/eventList">
+          <button class="btn bg-[#ea454c] text-white w-48 h-12 rounded-3xl">Explore events
+                    </button>    
+        </router-link>
+      </div>
+    </div>
 </template>
 
 <script>
