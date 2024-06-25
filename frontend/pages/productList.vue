d2e17127e198d8ed2d59d7917a56d601a263735f
yashpatil641 <yashbhadane240@gmail.com>
2024-06-25 13:21:39 +0530

    feat(frontend):Add products page

---
+++ frontend/pages/productList.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="m-14">
+      <div class="flex flex-col gap-5 items-center">
+        <h1 class="poppins font-bold w-full text-center text-4xl">OUR <span class="text-[#EA454C]">PRODUCTS</span></h1>
+        <div class="poppins text-base font-normal  flex gap-1 text-[#6C6C6C] ">Total <span class="font-semibold">
+            {{ productlists.length }} </span> products </div>
+      </div>
+  
+      <div class="flex justify-center items-center flex-col gap-8 mt-5 lg:flex-row ">
+        <div class="flex flex-wrap justify-center gap-8 bg-red px-10 py-5">
+          <NuxtLink v-for="data in productlists" :to="`/products/${data.id}`" :key="data.id">
+            <MerchCard
+              :title= "data.title"
+              :type= "data.type"
+              :imageUrl= "data.product_images" 
+              :seller = "data.seller"
+              :price = "data.price"
+              :description = "data.description"
+              :colors= "data.colors"
+              :sizes= "data.sizes"
+              :tags = "data.tags"
+            />
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script setup>
+  import MerchCard from '@/components/MerchCard.vue';
+  const config = useRuntimeConfig();
+  const productlists = ref([]);
+   
+  async function fetchEventData() {
+     try {
+      const response = await $fetch(`${config.public.API_BASE_URL}/api/products/`);
+      productlists.value = response.map(product => {
+        return {
+          ...product,
+          product_images: product.product_images.map(img => ({
+            ...img,
+            image: `${config.public.API_BASE_URL}${img.image}`
+          }))
+        };
+      });
+    }
+    catch (error) {
+      console.error('Error fetching event data', error);
+    }
+  }
+  onMounted(() => fetchEventData());
+  </script>
+  
+  
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  .poppins {
+    font-family: "Poppins", sans-serif;
+    font-smooth: always;
+  }
+  </style>
