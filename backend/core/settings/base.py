cdfbd74e45c035d188ed0af636c69daf917b103a
Kushagra Gupta <kushagragupta625@gmail.com>
2025-06-07 00:28:24 +0530

    adds django filters to the REST_FRAMEWORK config

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -47,7 +47,12 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 MIDDLEWARE = [
